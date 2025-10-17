6cb1055b80e3db95a6e309243bd9545999ca44b9
Javier Sánchez Mendoza <code@javier1nc.com>
2025-10-17 12:26:09 -0600

    feat: nav was added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon-sat.ico';
 
+	import { CalendarIcon, CircleUserIcon, MenuIcon, SearchIcon } from '@lucide/svelte';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+
 	import Header from '../lib/components/Layout/Header.svelte';
 	import Footer from '../lib/components/Layout/Footer.svelte';
 
@@ -16,7 +19,28 @@
 <div class="grid min-h-dvh grid-rows-[auto_minmax(0,1fr)_auto] overflow-x-hidden">
 	<!-- Header (responsive across all screen sizes) -->
 	<Header />
-	<div class="m-0 flex min-h-[3rem] flex-row justify-between bg-surface-800 px-4 sm:px-6"></div>
+
+	<AppBar class="-mt-[6rem] bg-[#611232] ">
+		<AppBar.Toolbar class="grid-cols-[1fr_2fr_1fr] ">
+			<AppBar.Lead>
+				<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></button>
+			</AppBar.Lead>
+			<AppBar.Headline class="flex justify-center">
+				<p>Headline</p>
+			</AppBar.Headline>
+			<AppBar.Trail class="justify-end">
+				<button type="button" class="btn-icon hover:preset-tonal"
+					><SearchIcon class="size-6" /></button
+				>
+				<button type="button" class="btn-icon hover:preset-tonal"
+					><CalendarIcon class="size-6" /></button
+				>
+				<button type="button" class="btn-icon hover:preset-tonal"
+					><CircleUserIcon class="size-6" /></button
+				>
+			</AppBar.Trail>
+		</AppBar.Toolbar>
+	</AppBar>
 
 	<!-- Main content area with mobile-first responsive design -->
 	<div class="w-full overflow-x-hidden">
