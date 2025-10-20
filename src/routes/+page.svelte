26fa1e471a9438e837aa904b256e666e5e2c4968
Javier Sánchez Mendoza <code@javier1nc.com>
2025-10-20 11:43:07 -0600

    wip: status quo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,23 @@
-<main class="mobile-spacing min-h-0 bg-transparent py-2 text-black sm:py-4 lg:py-6">
-
+<main
+	class="mobile-spacing scroll-y-hidden min-h-0 overflow-y-hidden bg-transparent py-2 text-black sm:py-4 lg:py-6"
+>
 	<div class="w-full space-y-4">
 		<h2 class="text-xl font-bold">Aviso de destino de los ingresos retornados al país</h2>
 		<hr class="hr border-t-2" />
 		<div class="flex items-center gap-4">
-		<!-- A standard button -->
-		
+			<!-- A standard button -->
 
-		<div class="w-full space-y-4 ">
-			<div class="flex justify-between items-center gap-4">
-				<!-- A standard button -->
-				<a type="button" class="btn preset-filled border-2" href="/instructions">Instrucciones</a>
-				<a type="button" class="btn preset-filled border-2 border-[#9b2247] text-[#9b2247] hover:bg-[#9b2247] hover:text-white" href="/about">Enviar declaración</a>
+			<div class="w-full space-y-4">
+				<div class="flex items-center justify-between gap-4">
+					<!-- A standard button -->
+					<a type="button" class="btn border-2 preset-filled" href="/instructions">Instrucciones</a>
+					<a
+						type="button"
+						class="btn border-2 border-[#9b2247] preset-filled text-[#9b2247] hover:bg-[#9b2247] hover:text-white"
+						href="/about">Enviar declaración</a
+					>
+				</div>
 			</div>
 		</div>
 	</div>
-	
 </main>
